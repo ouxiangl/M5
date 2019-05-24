95b200b7c25839ca92cca7422f27d377a301d27d
Ouxiang Lin <ouxiangl1998@gmail.com>
2019-05-24 17:43:15 +0200

    Ex16

--- src/Client.java
+++ src/Client.java
@@ -37,13 +37,11 @@ public class Client {
     }
 
     public String informe() {
-        double total = 0;
         int bonificacions = 0;
         String resultat = "Informe de lloguers del client " +
             getNom() +
             " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-            double quantitat = lloguer.quantitat();
             
             // afegeix lloguers freqüents
             bonificacions +=lloguer.bonificacions();
@@ -52,13 +50,19 @@ public class Client {
                 lloguer.getVehicle().getMarca() +
                 " " +
                 lloguer.getVehicle().getModel() + ": " +
-                (quantitat * 30) + "€" + "\n";
-            total += quantitat * 30;
+                (lloguer.quantitat() * 30) + "€" + "\n";
         }
 
         // afegeix informació final
-        resultat += "Import a pagar: " + total + "€\n" +
+        resultat += "Import a pagar: " + importTotal() + "€\n" +
             "Punts guanyats: " + bonificacions + "\n";
         return resultat;
+    }
+    public double importTotal() {
+        double total = 0;
+        for (Lloguer lloguer: lloguers) {
+            total += lloguer.quantitat() * 30;
+        }
+        return total;
     }
 }
